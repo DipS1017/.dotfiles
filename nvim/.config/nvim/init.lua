7020d0e299bdbf4cca163ebf9f3b49f7a5228d4a
dips <dipeshshrestha910@gmail.com>
2025-02-04 22:31:43 +0545

    barbeque nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,6 +10,24 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+-- format on save
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then
+            return
+        end
+
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                end,
+            })
+        end
+    end,
+})
 -- vim.opt.termguicolors=true
 require("vim-options")
 require("lazy").setup("plugins")
