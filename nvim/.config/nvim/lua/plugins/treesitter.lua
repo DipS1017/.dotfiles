000bd7f82f2e5e9e967203bd6cc56e788098243e
dips1017 <dipeshshrestha910@gmail.com>
2026-01-05 19:20:19 +0545

    feat: added a cold fold feature on neovim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,12 +3,23 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+      _G.treesitter_foldtext = function()
+        local start_line = vim.fn.getline(vim.v.foldstart)
+        local count = vim.v.foldend - vim.v.foldstart + 1
+        return string.format("%s  (%d lines)", start_line, count)
+      end
+
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.opt.foldtext = "v:lua.treesitter_foldtext()"
+      vim.opt.foldlevel = 99
+      vim.opt.foldcolumn = "0"
     end
   }
 }
